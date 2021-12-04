26afd93883b4c9b7aca44ebc5b0144657fb81984
LeMoonStar <webmaster@unitcore.de>
2021-12-04 18:03:41 +0100

    ported day 1

--- src/days/d01.rs
+++ src/days/d01.rs
@@ -2,7 +2,17 @@ use super::{Day, DayImpl};
 
 const CURRENT_DAY: u8 = 1;
 
-type Data = Vec<u64>;
+#[derive(Debug, Copy, Clone)]
+enum Command {
+    Forward(u64),
+    Down(u64),
+    Up(u64),
+}
+
+pub struct Data {
+    input: Vec<Command>,
+}
+
 impl DayImpl<Data> for Day<CURRENT_DAY> {
     fn init_test() -> (Self, Data)
     where
@@ -21,19 +31,48 @@ impl DayImpl<Data> for Day<CURRENT_DAY> {
     {
         (
             Self {},
-            input
-                .lines()
-                .filter(|v| v.len() != 0)
-                .map(|v| v.parse::<u64>().expect("error while parsing input."))
-                .collect(),
+            Data {
+                input: input
+                    .lines()
+                    .filter(|v| v.len() != 0)
+                    .map(|v| match v.len() {
+                        4 => Command::Up(v.get(v.len() - 1..).unwrap().parse::<u64>().unwrap()),
+                        6 => Command::Down(v.get(v.len() - 1..).unwrap().parse::<u64>().unwrap()),
+                        9 => Command::Forward(v.get(v.len() - 1..).unwrap().parse::<u64>().unwrap()),
+                        _ => panic!("malformed input."),
+                    })
+                    .collect(),
+            }
         )
     }
 
     fn one(&self, data: &mut Data) -> u64 {
-        data.len() as u64
+        let mut depth: u64 = 0;
+        let mut horizontal_pos: u64 = 0;
+        for c in &data.input {
+            match c {
+                Command::Down(v) => depth = depth + (*v as u64),
+                Command::Up(v) => depth = depth - (*v as u64),
+                Command::Forward(v) => horizontal_pos = horizontal_pos + (*v as u64),
+            }
+        }
+        (depth * horizontal_pos) as u64
     }
 
     fn two(&self, data: &mut Data) -> u64 {
-        data.len() as u64
+        let mut depth: u64 = 0;
+        let mut aim: u64 = 0;
+        let mut horizontal_pos: u64 = 0;
+        for c in &data.input {
+            match c {
+                Command::Down(v) => aim = aim + v,
+                Command::Up(v) => aim = aim - v,
+                Command::Forward(v) => {
+                    horizontal_pos = horizontal_pos + v;
+                    depth = depth + v * aim;
+                }
+            }
+        }
+        depth * horizontal_pos
     }
 }
