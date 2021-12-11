023b60adf102e29ba494a02c8d86c78d85b351ab
LeMoonStar <webmaster@unitcore.de>
2021-12-11 12:28:24 +0100

    added solution to day 11

--- src/days/d11.rs
+++ src/days/d11.rs
@@ -2,14 +2,78 @@ use super::{Day, DayImpl};
 
 const CURRENT_DAY: u8 = 11;
 
-type Data = Vec<u64>;
+fn get_pos(data: &mut Data, pos: (usize, usize), offset: (isize, isize)) -> Option<&mut u8> {
+    let new_pos = (pos.0 as isize + offset.0, pos.1 as isize + offset.1);
+    if new_pos.0 < 0 || new_pos.1 < 0 {
+        return None;
+    }
+
+    let new_pos = (new_pos.0 as usize, new_pos.1 as usize);
+
+    match data.get_mut(new_pos.0) {
+        Some(r) => r.get_mut(new_pos.1),
+        None => None,
+    }
+}
+
+fn flash(data: &mut Data, pos: (usize, usize), offset: (isize, isize), by_neigbour: bool) -> u64 {
+    if let Some(this) = get_pos(data, pos, offset) {
+        if *this == 0 {
+            return 0;
+        }
+        if by_neigbour {
+            *this += 1;
+        }
+
+        if *this > 9 {
+            *this = 0;
+            1 + flash(data, pos, (offset.0 + -1, offset.1 + -1), true)
+                + flash(data, pos, (offset.0 + -1, offset.1 + 1), true)
+                + flash(data, pos, (offset.0 + 1, offset.1 + -1), true)
+                + flash(data, pos, (offset.0 + 1, offset.1 + 1), true)
+                + flash(data, pos, (offset.0 + -1, offset.1 + 0), true)
+                + flash(data, pos, (offset.0 + 1, offset.1 + 0), true)
+                + flash(data, pos, (offset.0 + 0, offset.1 + -1), true)
+                + flash(data, pos, (offset.0 + 0, offset.1 + 1), true)
+        } else {
+            0
+        }
+    } else {
+        0
+    }
+}
+
+fn simulate(data: &mut Data, steps: usize) -> u64 {
+    let mut flashes = 0;
+
+    for s in 0..steps {
+        let mut buf = data.clone();
+        for row in 0..buf.len() {
+            for col in 0..buf[row as usize].len() {
+                buf[row][col] += 1;
+            }
+        }
+
+        for row in 0..buf.len() {
+            for col in 0..buf[row as usize].len() {
+                flashes += flash(&mut buf, (row, col), (0, 0), false);
+            }
+        }
+
+        *data = buf;
+    }
+
+    flashes
+}
+
+type Data = Vec<Vec<u8>>;
 impl DayImpl<Data> for Day<CURRENT_DAY> {
     fn init_test() -> (Self, Data) {
         Self::init(&include_str!("test_inputs/test11.txt").to_owned())
     }
 
     fn expected_results() -> (u64, u64) {
-        (0, 0)
+        (1656, 195)
     }
 
     fn init(input: &str) -> (Self, Data) {
@@ -17,16 +81,30 @@ impl DayImpl<Data> for Day<CURRENT_DAY> {
             Self {},
             input
                 .lines()
-                .map(|v| v.parse::<u64>().expect("error while parsing input."))
+                .map(|v| {
+                    v.chars()
+                        .map(|v| v.to_digit(10).expect("error while parsing input.") as u8)
+                        .collect()
+                })
                 .collect(),
         )
     }
 
     fn one(&self, data: &mut Data) -> u64 {
-        data.len() as u64
+        simulate(data, 100)
     }
 
     fn two(&self, data: &mut Data) -> u64 {
-        data.len() as u64
+        let mut steps = 0;
+
+        loop {
+            steps += 1;
+            let flashes = simulate(data, 1);
+            if flashes == (data.len() * data[0].len()) as u64 {
+                break;
+            }
+        }
+
+        steps
     }
 }
